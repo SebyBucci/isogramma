8f44420c8a18f9ee841262509ad77b2602dd39b3
Sebastian Bucci <127589946+SebyBucci@users.noreply.github.com>
2023-05-06 09:45:33 +0200

    Add files via upload

---
+++ bucci.sebastian.3i.isogrammma/Isogramma.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+
+
+public static class Isogramma
+{
+    public static bool Verifica(string word)
+    {
+     var Alphabet = new int [26];
+        foreach(var Letter in word.ToLower())
+        {
+            if (Char.IsLetter(Letter))
+            {
+                Alphabet [Letter - 'a']++;
+                if (Alphabet[Letter - 'a'] > 1)
+                    return false;
+            }
+        }
+        return true;
+    }
+
+}
+
